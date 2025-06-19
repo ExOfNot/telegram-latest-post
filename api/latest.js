a8a19777d80af16bc48bed2020bce08069d9f11e
Sergey Tikhonenko <expert@veks2000.ru>
2025-06-20 00:07:23 +0300

    Initial commit

---
+++ api/latest.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://t.me/s/ofs_stom');
+    const html = await response.text();
+
+    const dom = new JSDOM(html);
+    const a = dom.window.document.querySelector('a.tgme_widget_message_date');
+    if (!a) {
+      return res.status(500).json({ error: 'Не удалось найти ссылку на пост' });
+    }
+    const href = a.href;
+    const id = href.split('/').pop();
+
+    res.status(200).json({ id: parseInt(id, 10) });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
